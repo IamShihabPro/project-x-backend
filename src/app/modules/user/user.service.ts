f97bb85fca76ffcb101120ac2f6270044c20e064
IamShihabPro <mshihab.pro@gmail.com>
2024-10-04 01:24:24 +0600

    post create

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -5,9 +5,6 @@ import { User } from "./user.model";
 import { sendImageToCloudinary } from "../utils/sendImageToCloudinary";
 import AppError from "../../errors/AppError";
 import httpStatus from "http-status";
-import bcrypt from 'bcryptjs';
-import jwt from "jsonwebtoken";
-import config from "../../config";
 
 const signUpUserIntoDB = async (file: Express.Multer.File | undefined, payload: TSignUp) => {
   const session = await mongoose.startSession();
@@ -46,51 +43,9 @@ const signUpUserIntoDB = async (file: Express.Multer.File | undefined, payload: 
     await session.abortTransaction();
     throw new AppError(httpStatus.INTERNAL_SERVER_ERROR, err.message || 'Transaction failed');
   } finally {
-    session.endSession(); // Ensure session ends in both success and failure cases
+    session.endSession();
   }
 };
-
-
-
-// const signInUser = async (payload: TSignin) => {
-//   const isUserExists = await User.findOne({ email: payload.email }).select('+password');
-  
-//   if (!isUserExists) {
-//       throw new AppError(httpStatus.NOT_FOUND, "This user is not found");
-//   }
-
-//   const isPasswordMatch = await bcrypt.compare(payload.password, isUserExists.password);
-  
-//   if (!isPasswordMatch) {
-//       throw new AppError(httpStatus.UNAUTHORIZED, "Incorrect password");
-//   }
-
-//   const isDeleted = isUserExists?.isDeleted;
-
-//   if (isDeleted) {
-//     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
-//   }
-
-//   const jwtPayload = {
-//       email: isUserExists.email,
-//       role: isUserExists.role,
-//   };
-
-//   const token = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
-//       expiresIn: config.jwt_access_expires_in,
-//       });
-
-//   const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret as string,
-//           {
-//             expiresIn: config.jwt_refresh_expires_in,
-//           }
-//   );
-
-//   const result = await User.findById(isUserExists._id).select('-password');
-
-//   return {result, token, refreshToken};
-// }
-
 
 
 
